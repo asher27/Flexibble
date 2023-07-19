0af2277e70e318c5ced986f3dfcead825e79bd13
David Hwang <asher1225@gmail.com>
2023-07-19 23:32:41 +0900

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['cloudinary']
+  }
+};
 
 module.exports = nextConfig
